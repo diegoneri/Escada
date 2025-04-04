54787f09b841627e6aac6b54e8b650496ab26b52
Diego Neri <diego.neri@etec.sp.gov.br>
2025-04-04 19:45:37 -0300

    Finaliza exercício

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿double comprimentoEscada, altura;
+double anguloEmGraus, anguloEmRadianos;
+
+Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.BackgroundColor = ConsoleColor.Gray;
+
+Console.WriteLine("--- Escada ---\n\n");
+
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.BackgroundColor = ConsoleColor.Gray;
+
+Console.Write("Informe o comprimento da escada: ");
+comprimentoEscada = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Informe o ângulo (em graus) da posição da escada: ");
+anguloEmGraus = Convert.ToDouble(Console.ReadLine()!);
+
+Console.ResetColor();
+
+anguloEmRadianos = Math.PI * anguloEmGraus / 180;
+altura = comprimentoEscada * Math.Sin(anguloEmRadianos);
+
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.BackgroundColor = ConsoleColor.Gray;
+
+Console.WriteLine($"Uma escada de {comprimentoEscada}m a {anguloEmGraus}° alcança {altura:N2}m");
+
+Console.ResetColor();
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Escada
 Fonte: https://github.com/ermogenes/aulas-programacao-csharp/blob/master/exercises/sub-rotinas.md#exerc%C3%ADcio-escada
+
+---
+
+## Exercício `Escada`
+
+Uma escada ficará enconstada em uma parede caso forme entre ela e o chão um ângulo menor do que 90˚.
+
+Escreva uma função que calcule a altura alcançada pela escada dados o comprimento da escada (em m) e o ângulo em relação ao chão (em graus).
+
+_Converta o ângulo em graus para radianos, fazendo `(π graus) / 180`._
+
+Para calcular a altura, saiba que o comprimento da escada equivale à razão entre a altura e o seno do ângulo.
+
+Em ![](escada-1.svg), `b = c.senβ`.
+
+Valores para teste:
+escada | ângulo | parede
+--- | --- | ---
+4 | 70 | 3,76
+4 | 45 | 2,83
+5 | 70 | 4,70
+
+Mais valores de teste [aqui](https://www.calculat.org/pt/area-perimetro/triangulo-retangulo.html).
+
+---
